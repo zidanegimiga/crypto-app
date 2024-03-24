ab851586f91a74228bc51b0c4d56ec465d6ac15c
zidane <zidane@nathandigital.com>
2024-03-24 08:41:20 +0300

    App set up and screens

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/Home';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SignUp from './screens/Auth/SignUp';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="SignUp" component={SignUp} options={{
+          header: ()=> {}
+        }}/>
+        <Stack.Screen name="Home" component={Home} options={{
+          header: ()=> {}
+        }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/Home/index.jsx
@@ -0,0 +1,12 @@
+import { SafeAreaView, View, Text } from 'react-native'
+import React from 'react'
+
+const Home = ({navigation}) => {
+  return (
+    <SafeAreaView>
+      <Text>Home page</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Home
